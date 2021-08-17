26d8429ca15833e2f22bab58239b93783cae0e8c
Guangchuang Yu <guangchuangyu@gmail.com>
2021-08-17 16:52:52 +0800

    update vignette

--- R/utilities.R
+++ R/utilities.R
@@ -16,18 +16,3 @@ get_aes_var <- function(mapping, var) {
     tail(res, 1)
 }
 
-##' load function from package
-##'
-##'
-##' @title get_fun_from_pkg
-##' @param pkg package
-##' @param fun function
-##' @return function
-##' @export
-##' @author guangchuang yu
-get_fun_from_pkg <- function(pkg, fun) {
-    ## requireNamespace(pkg)
-    ## eval(parse(text=paste0(pkg, "::", fun)))
-    require(pkg, character.only = TRUE)
-    eval(parse(text = fun))
-}
