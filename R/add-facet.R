2e09151cca1d110a3a319ea7c0bf9459710557c3
Guangchuang Yu <guangchuangyu@gmail.com>
2021-09-03 17:06:53 +0800

    side and angle parameter for add_facet()

--- R/add-facet.R
+++ R/add-facet.R
@@ -4,6 +4,8 @@
 ##' @title add_facet
 ##' @param plot a ggplot object
 ##' @param label a string to label the plot
+##' @param side to label the plot at which side, either 't' (top) or 'r' (right)
+##' @param angle angle of the facet label. Default is 0 for side='t' and -90 for side='r'.
 ##' @return a ggplot with facet label
 ##' @importFrom ggplot2 theme
 ##' @importFrom ggplot2 margin
@@ -13,11 +15,23 @@
 ##' @importFrom ggplot2 facet_grid
 ##' @export
 ##' @author Guangchuang Yu
-add_facet <- function(plot, label) {
-    plot + facet_grid(eval(parse(text=paste0("~'", eval(label), "'")))) +
+add_facet <- function(plot, label, side = 't', angle = NULL) {
+    side <- match.arg(side, c('t', 'r'))
+    lb <- paste0("'", eval(label), "'")
+    if (side == 't') {
+        lb <- paste0('~', lb)
+    } else {
+        lb <- paste0(lb, '~.')
+        if (is.null(angle))  angle <- -90
+    }
+
+    plot + facet_grid(eval(parse(text=lb))) +
         theme(strip.background = element_rect(fill='grey85', colour = NA),
-              strip.text = element_text(colour = 'grey10', size = rel(0.8),
+              strip.text = element_text(colour = 'grey10',
+                                        size = rel(0.8),
+                                        angle = angle,
                                         margin = margin(4.4, 4.4, 4.4, 4.4))
               )
 }
+
 
