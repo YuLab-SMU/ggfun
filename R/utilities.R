53cafc0ac4b7f09a37fe66170001cb8f53b75240
Guangchuang Yu <guangchuangyu@gmail.com>
2021-09-16 11:04:07 +0800

    ggbreak2ggplot, is.ggbreak and is.ggtree

--- R/utilities.R
+++ R/utilities.R
@@ -16,3 +16,61 @@ get_aes_var <- function(mapping, var) {
     tail(res, 1)
 }
 
+
+
+
+##' convert a ggbreak object to a ggplot object
+##'
+##'
+##' @title ggbreak2ggplot
+##' @param plot a ggbreak object
+##' @return a ggplot object
+##' @export
+##' @author Guangchuang Yu
+ggbreak2ggplot <- function(plot) {
+    ggplotify::as.ggplot(grid.draw(plot, recording = FALSE))
+}
+
+##' check whether a plot is a ggbreak object (including 'ggbreak', 'ggwrap' and 'ggcut' that defined in the 'ggbreak' package)
+##'
+##'
+##' @title is.ggbreak
+##' @rdname is-ggbreak
+##' @param plot a plot obejct
+##' @return logical value
+##' @export
+##' @author Guangchuang Yu
+is.ggbreak <- function(plot) {
+    if (inherits(plot, 'ggbreak') ||
+        inherits(plot, 'ggwrap') ||
+        inherits(plot, 'ggcut')
+        ) return(TRUE)
+
+    return(FALSE)
+}
+
+
+##' test whether input object is produced by ggtree function
+##'
+##'
+##' @title is.ggtree
+##' @param x object
+##' @return TRUE or FALSE
+##' @export
+##' @author Guangchuang Yu
+## copy from treeio
+is.ggtree <- function(x) {
+    if (inherits(x, 'ggtree')) return(TRUE)
+
+    if (!inherits(x, 'gg')) return(FALSE)
+
+    ## to compatible with user using `ggplot(tree) + geom_tree()`
+
+    tree_layer <- vapply(x$layers,
+                         function(y) {
+                             any(grepl("StatTree", class(y$stat)))
+                         },
+                         logical(1)
+                         )
+    return(any(tree_layer))
+}
