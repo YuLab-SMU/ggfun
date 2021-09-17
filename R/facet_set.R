3a58e863a528c7ed07fd3801bda840999b235bba
xiangpin <xshuangbin@163.com>
2021-09-17 16:49:17 +0800

    update facet_manual and renamed it to facet_set

--- R/facet_set.R
+++ R/facet_set.R
@@ -1,12 +1,12 @@
 ##' add a facet label to a ggplot or change facet label of a ggplot
 ##'
-##' @title facet_manual
+##' @title facet_set
 ##' @param label a character or a named vector to label the plot
 ##' @param side to label the plot at which side, either 't' (top) or 'r' (right)
 ##' @param angle angle of the facet label. Default is 0 for side='t' and -90 for side='r'.
 ##' @return a ggplot with facet label
 ##' @export
-facet_manual <- function(label, side="t", angle = NULL){
+facet_set <- function(label, side="t", angle = NULL){
     side <-  match.arg(side, c('top', 'right'))
 
     structure(list(
@@ -14,6 +14,6 @@ facet_manual <- function(label, side="t", angle = NULL){
         side  = side,
         angle = angle
         ),
-        class = "facet_manual"
+        class = "facet_set"
     )
 }
--- R/method-ggplot-add.R
+++ R/method-ggplot-add.R
@@ -1,7 +1,7 @@
 #' @importFrom ggplot2 ggplot_add
-#' @method ggplot_add facet_manual
+#' @method ggplot_add facet_set
 #' @export
-ggplot_add.facet_manual <- function(object, plot, object_name){
+ggplot_add.facet_set <- function(object, plot, object_name){
     if (object$side == 'right' && is.null(object$angle)) {
         object$angle <- -90
     }
@@ -14,16 +14,26 @@ build_new_plot <- function(object, plot){
     flag.params <- TRUE
     if (!inherits(plot$facet, "FacetNull")){
         if (inherits(object$label, "labeller") || !is.null(names(object$label))){
+            facet.fun <- eval(parse(text=class(plot$facet)[1]))
             facet.obj <- ggplot2::ggproto(NULL, 
-                                      eval(parse(text=class(plot$facet)[1])), 
+                                      facet.fun, 
                                       shrink = plot$facet$shrink,
                                       params = plot$facet$params
                          )
-            if (inherits(object$label, "labeller")){
-                facet.obj$params$labeller <- object$label
-            }else{
-                facet.obj$params$labeller <- ggplot2::as_labeller(object$label)
+            if (!is.null(plot$facet$strip)){
+                facet.obj$strip <- plot$facet$strip
             }
+            strip.labels <- extract_strip_label(facet=facet.fun, plot=plot)
+            if (inherits(object$label, "labeller")){
+                tmp.label <- extract_strip_label(facet=facet.fun, plot=plot, labeller=object$label)
+                names(tmp.label) <- names(strip.labels)
+                object$label <- tmp.label[!is.na(tmp.label)]
+            }
+            newnm <- intersect(names(object$label), names(strip.labels))
+            if (length(newnm) > 0){
+                strip.labels[match(newnm, names(strip.labels))] <- object$label[match(newnm, names(object$label))]
+            }
+            facet.obj$params$labeller <- ggplot2::as_labeller(strip.labels)
             flag.params <- FALSE
         }else{
             plot <- ggplotify::as.ggplot(plot)
@@ -53,3 +63,4 @@ build_new_plot <- function(object, plot){
     }
     return (plot)
 }
+
